087161abaa39bccb35f9880e79a8ffd58632cdb5
dxx566408-oss <dxx566408@gmail.com>
2026-01-10 14:23:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import os
-from flask import Flask, render_template
 import threading
-from discord.ext import commands
+from flask import Flask, render_template
 import discord
+from discord.ext import commands
 
+# 1. إعداد السيرفر
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # هذا السطر هو اللي بيخلي الموقع يفتح ملف index.html اللي سويناه
     return render_template('index.html')
 
 def run():
-    # غيرنا الرقم لـ 5000 ليتطابق مع ما يبحث عنه Render
+    # تأكدنا أن البورت 5000 ليتوافق مع ريندر
     app.run(host='0.0.0.0', port=5000)
 
 def keep_alive():
     t = threading.Thread(target=run)
     t.start()
 
+# 2. إعداد البوت
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'✅ {bot.user} Online and Dashboard Ready!')
-    await bot.tree.sync()
+    print(f'✅ {bot.user} Online!')
+    try:
+        await bot.tree.sync()
+    except Exception as e:
+        print(e)
 
-keep_alive()
-token = os.environ.get("DISCORD_TOKEN")
-bot.run(token)
-
-
-
+# 3. التشغيل
+if __name__ == "__main__":
+    keep_alive()
+    token = os.environ.get("DISCORD_TOKEN")
+    bot.run(token)
